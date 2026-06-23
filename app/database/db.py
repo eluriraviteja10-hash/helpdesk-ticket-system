b0f87c9670adcc11fc467f37f05593a39a20d53c
Eluri Raviteja <eluriraviteja10@gmail.com>
2026-06-23 18:36:13 +0530

    Use DATABASE_URL environment variable

--- app/database/db.py
+++ app/database/db.py
@@ -5,14 +5,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql+psycopg2://"
-    f"{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}:"
-    f"{os.getenv('DB_PORT')}/"
-    f"{os.getenv('DB_NAME')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
