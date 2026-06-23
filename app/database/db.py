3e9684d7caecc3ca688d6043102149c5c7f707dd
Eluri Raviteja <eluriraviteja10@gmail.com>
2026-06-23 18:54:11 +0530

    Configure production database

--- app/database/db.py
+++ app/database/db.py
@@ -7,6 +7,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg2://",
+        1
+    )
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
